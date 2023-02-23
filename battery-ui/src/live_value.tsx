2f8cdb4bbff82acdb48c08ab92e3dcc25014f5bb
jordanliang5 <121711957+jordanliang5@users.noreply.github.com>
2023-02-24 00:33:16 +1100

    frontend changes

--- battery-ui/src/live_value.tsx
+++ battery-ui/src/live_value.tsx
@@ -5,14 +5,48 @@ interface TemperatureProps {
   temp: number;
 }
 
+function tempColour(temp: number): string {
+  var colour = 'white';
+  if (40 < temp && 60 > temp) {
+    colour = 'green-yellow';
+  } else if ((30 <= temp && 40 >= temp) ||
+             (60 <= temp && 70 >= temp)) {
+    colour = 'yellow';
+  } else if ((20 <= temp && 30 >= temp) ||
+             (70 <= temp && 80 >= temp)) {
+    colour = 'orange';
+  } else {
+    colour = 'red';
+  }
+
+  return colour;
+}
+
+function tempDiff(temp: number): string {
+  var diff = "";
+  if (temp < 20) {
+    diff = 'Too low!';
+  } else if (temp > 80) {
+    diff = 'Too high!';
+  }
+  return diff;
+}
+
 function LiveValue({ temp } : TemperatureProps) {
 
-  let valueColour = 'white';
+  let valueColour = tempColour(temp);
 
   return (
-      <header className="live-value" style={{ color : valueColour }}>
-        {`${temp.toString()}°C`}
-      </header>
+      <div className='temperature-wrapper'>
+        <div className='temperature-border' style={{ color : valueColour }}>
+          <div className='live-value-wrapper'>
+            <header className="live-value" style={{ color : valueColour }}>
+              {`${temp.toString()}°C`}
+            </header>
+            <div className='temperature-warning'>{tempDiff(temp)}</div>
+          </div>
+        </div>
+      </div>
   );
 }
 
--- brainstorming.md
+++ brainstorming.md
@@ -11,3 +11,23 @@ Essentially, for the first datapoint, set its timestamp as the start of the inte
 Once the number of out of range flags hits 3, check how long the interval between the first datapoint and the last is. If it is within 5 seconds, then write to the report.
 
 Reset the counter and start interval once we do this check.
+
+Task 3
+Temp range colour
+
+20 30 40 50 60 70 80
+
+40 < temp < 60 green
+
+30 <= temp <= 40 || 60 <= temp <= 70 yellow
+
+20 <= temp <= 30 || 70 <= temp <= 80 yellow
+
+temp < 20 || temp > 80 red
+
+Also say message when out of range:
+OUT OF RANGE: Too high/Too low
+
+Potentially:
+Store the last 10 temperatures and display their average
+Plot temperatures on a graph
